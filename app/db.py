03aaa4f21326f7e2fff93ba25351ec8dd8642ca3
bni27 <ian.bakst@oxfordglobalprojects.com>
2024-07-13 13:48:54 -0400

    trying a load

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from google.cloud.sql.connector import Connector
 from sqlalchemy import create_engine, MetaData, text
@@ -34,6 +35,83 @@ def select_data(table_name: str, schema_name: str = "prod") -> list[dict]:
         a = connection.execute(text(f'SELECT * FROM "{schema_name}"."{table_name}"'))
     cols = [i for i in a.keys()]
     return [{c: r for r, c in zip(row, cols)} for row in a.all()]
+
+
+def load_raw(file_path: Path):
+    schema = "raw"
+    table_name = file_path.name.split(".")[0]
+    drop_table(table_name, schema)
+    load_data_into_table(file_path)
+
+
+def drop_table(table_name: str, schema: str):
+    with get_pool().connect() as connection:
+        connection.execute(text(f"DROP TABLE IF EXISTS {schema}.{table_name}"))
+        connection.commit()
+
+
+def create_table_from_headers(headers: list[str], table_name: str, schema: str = "raw"):
+    with get_pool().connect() as connection:
+        col_str = gen_col_str(headers, ["project_id", "sample"])
+        connection.execute(
+            text(
+                f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table_name} 
+                ( {col_str}
+                )
+                """
+            )
+        )
+        connection.commit()
+
+
+def load_data_into_table(file_path: Path):
+    table_name = file_path.name.split('.')[0]
+    with open(file_path, 'r') as f:
+        header_line = f.readline()
+        headers = header_line.split(',')
+        create_table_from_headers(headers, table_name)
+        conn = get_pool().raw_connection()
+        cursor = conn.cursor()
+        cmd = f'COPY {table_name}({headers}) FROM STDIN WITH (FORMAT CSV, HEADER FALSE)'
+        cursor.copy_expert(cmd, f)
+        conn.commit()
+
+
+def gen_col_str(
+    columns: list[str],
+    primary_key: str | list[str] | None = None,
+    default_type: str = "varchar",
+    special_type: dict[str, str] | None = None,
+) -> str:
+    column_strings = []
+    if special_type is None:
+        special_type = {}
+    column_types = {}
+    for k, v in special_type.items():
+        if isinstance(v, str):
+            column_types[v] = k
+        else:
+            column_types.update({i: k for i in v})
+    if primary_key is None:
+        primary_key = [columns[0]]
+    if isinstance(primary_key, str):
+        primary_key = [primary_key]
+    for col in columns:
+        column_strings.append(col.lower())
+        if col.lower() in column_types:
+            column_strings[-1] += " " + column_types.get(col.lower(), "varchar")
+        else:
+            column_strings[-1] += " " + default_type
+        if col.lower() in primary_key:
+            column_strings[-1] += " NOT NULL"
+    if primary_key is None:
+        column_strings.append(f"PRIMARY KEY {columns[0]}")
+    elif isinstance(primary_key, str):
+        column_strings.append(f"PRIMARY KEY {primary_key}")
+    elif isinstance(primary_key, list):
+        column_strings.append(f"PRIMARY KEY ({', '.join(primary_key)})")
+    return ", ".join(column_strings)
 
 
 def union_prod():
--- app/routers/data.py
+++ app/routers/data.py
@@ -1,11 +1,14 @@
-from os.path import join
+import logging
+from os.path import exists, join, isdir, mkdir
+from pathlib import Path
 
-from fastapi import APIRouter, Depends, File, HTTPException, status, UploadFile
+from fastapi import APIRouter, Depends, File, status, UploadFile
 
-from app.db import select_data, union_prod
+from app.db import load_raw, select_data, union_prod
 from app.auth import AuthLevel, User, validate_api_key
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.get("/")
@@ -15,18 +18,27 @@ async def data(asset_class: str | None = None):
 
 @router.post("/uploadFile")
 def upload_file(
+    asset_class: str,
     file: UploadFile = File(),
-    api_key: User = Depends(validate_api_key),
+    verified: bool = False,
+    overwrite: bool = False,
+    authenticated_user: User = Depends(validate_api_key),
 ):
     bucket_mount: str = "/data"
-    if api_key.auth_level != AuthLevel.ADMIN:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You lack sufficient privileges for this action.",
-        )
+    required_perm = AuthLevel.ADMIN if (verified or overwrite) else AuthLevel.EDIT
+    authenticated_user.check_privilege(required_perm)
+    ver_folder = "verified" if verified else "unverified"
+    base_path = join(bucket_mount, ver_folder, asset_class)
+    if not isdir(base_path):
+        mkdir(base_path)
+    file_path = join(base_path, file.filename)
+    if exists(file_path) and not overwrite:
+        return {
+            "message": f"Cannot upload {asset_class}/{file.filename}, file already exists. Set overwrite flag if you are sure you want to overwrite."
+        }
     try:
         contents = file.file.read()
-        with open(join(bucket_mount, file.filename), "wb") as f:
+        with open(file_path, "wb") as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
@@ -36,8 +48,20 @@ def upload_file(
     return {"message": f"Successfully uploaded {file.filename}"}
 
 
-@router.post("/update", status_code=status.HTTP_204_NO_CONTENT)
-async def update(api_key: User = Depends(validate_api_key)):
+@router.post('/updateRaw')
+def update_raw():
+    file_path = Path(
+        '/data/verified/batteries_electrolyzer/batteries_electrolyzer.csv'
+    )
+    try:
+        load_raw(file_path)
+    except Exception as e:
+        logger.exception(e)
+
+
+@router.post("/updateVerified", status_code=status.HTTP_204_NO_CONTENT)
+async def update(authenticated_user: User = Depends(validate_api_key)):
+    authenticated_user.check_privilege()
     # load new raw datasets
     # process them to stage
     # union them to prod table
