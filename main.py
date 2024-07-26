dbcaf45d8e05d5852614e6cf16a8c1284e2f7de3
bni27 <ian.bakst@oxfordglobalprojects.com>
2024-07-25 22:46:55 -0400

    silly log error

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=os.environ.get("PORT", 8080), host="0.0.0.0")
+    uvicorn.run(app, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
