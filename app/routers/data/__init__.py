b95d7f0139e3620c239f612326428147a1753001
bni27 <ian.bakst@oxfordglobalprojects.com>
2024-12-30 21:17:25 -0500

    data router directory

---
+++ app/routers/data/__init__.py
@@ -0,0 +1,42 @@
+import logging
+from pathlib import Path
+
+from fastapi import APIRouter, Depends, File, HTTPException, status, UploadFile
+from fastapi.responses import FileResponse
+
+from app.auth import AuthLevel, User, validate_api_key
+from app.db import (
+    delete_raw_table,
+    delete_stage_table,
+    load_raw_data,
+    stage_data,
+    union_prod,
+)
+from app.filesys import (
+    build_asset_path,
+    build_raw_file_path,
+    get_data_files,
+    get_directories,
+)
+from app.pg import Record, row_count, select_data
+from app.pg import DateFormatError, DuplicateHeaderError, PrimaryKeysMissingError
+from app.sql import prod_table, stage_schema
+from app.table import DB_MGMT
+
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+@router.get("/")
+async def data(verified: bool = True, db: DB_MGMT):
+    return select_data(prod_table(verified), "prod")
+
+
+@router.post("/update", status_code=status.HTTP_204_NO_CONTENT)
+async def update(
+    verified: bool = True,
+    authenticated_user: User = Depends(validate_api_key),
+):
+    authenticated_user.check_privilege()
+    union_prod(verified)
