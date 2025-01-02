bd2a9ba551c1fc95277f8378569beb776f355cc0
bni27 <ian.bakst@oxfordglobalprojects.com>
2025-01-01 22:15:50 -0500

    remove default values

--- app/operations.py
+++ app/operations.py
@@ -63,10 +63,10 @@ def load_raw_data(file_path: Path, db: DatabaseManager):
         col_desc, dtypes = column_details(headers)
         db.create_new_table(table_name, schema, col_desc)
         with db.get_session() as session:
-            session.add(db.tables[schema][table_name](**{k: dtypes[k](v) for k, v in first_row.items()}))
+            session.add(db.tables[schema][table_name](**{k: v for k, v in first_row.items() if v != ""}))
             session.commit()
             for row in data:
-                session.add(db.tables[schema][table_name](**{k: dtypes[k](v) for k, v in row.items()}))
+                session.add(db.tables[schema][table_name](**{k: v for k, v in row.items() if v != ""}))
                 session.commit()
             
     return f"{schema}.{table_name}"
